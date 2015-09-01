2ef1a628d58a93fb33c4dbcb2e4065c56a401737
abhinay <abhinay212@gmail.com>
2015-09-01 05:17:18 -0700

    first commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/admin/');
+define('HTTP_CATALOG', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/admin/');
+define('HTTPS_CATALOG', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/admin/');
+define('DIR_SYSTEM', '/var/www/system/');
+define('DIR_LANGUAGE', '/var/www/admin/language/');
+define('DIR_TEMPLATE', '/var/www/admin/view/template/');
+define('DIR_CONFIG', '/var/www/system/config/');
+define('DIR_IMAGE', '/var/www/image/');
+define('DIR_CACHE', '/var/www/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/system/download/');
+define('DIR_UPLOAD', '/var/www/system/upload/');
+define('DIR_LOGS', '/var/www/system/logs/');
+define('DIR_MODIFICATION', '/var/www/system/modification/');
+define('DIR_CATALOG', '/var/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/catalog/');
+define('DIR_SYSTEM', '/var/www/system/');
+define('DIR_LANGUAGE', '/var/www/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/system/config/');
+define('DIR_IMAGE', '/var/www/image/');
+define('DIR_CACHE', '/var/www/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/system/download/');
+define('DIR_UPLOAD', '/var/www/system/upload/');
+define('DIR_MODIFICATION', '/var/www/system/modification/');
+define('DIR_LOGS', '/var/www/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
